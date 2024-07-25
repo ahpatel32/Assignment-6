1aecfbdc05aecd0b0288aaecb9dbed7c34ad17d0
Aakash Patel <ahpatel32@myseneca.ca>
2024-07-25 09:53:50 -0400

    commit1

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ const exphbs = require('express-handlebars');
         }    
         }
     }));
+    app.set("views", __dirname + "/views");
     app.set('view engine', '.hbs');
 
     //Static route to public folder - used to use the custom css files.
